123ae30511b7d3bda4d59f4c97bfe70b1e0f05fe
Otabek777 <53979293+Otabek777@users.noreply.github.com>
2022-05-13 16:34:05 +0500

    modal call

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 $('.banner .btn').click(function() {
     $('.modal').addClass('active');
+    $('body').addClass('hidden');
+});
+$('.header__phone .call').click(function() {
+    $('.modal-call').addClass('active');
+    $('body').addClass('hidden');
 });
 $('.modal__close').click(function() {
     $('.modal').removeClass('active');
+    $('.modal-call').removeClass('active');
+    $('body').removeClass('hidden');
 });
 $('.header__burgir').click(function() {
     $('.header__navbar').toggleClass('active');
